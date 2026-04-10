26a14bccbe6ed8c0f4271ceade3095444f906ca1
crazywhalecc <jesse2061@outlook.com>
2026-04-10 14:21:06 +0800

    Disable iouring on glibc build

--- src/globals/test-extensions.php
+++ src/globals/test-extensions.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
 $test_php_version = [
     // '8.1',
     // '8.2',
-    // '8.3',
+    '8.3',
     // '8.4',
     '8.5',
     // 'git',
@@ -26,12 +26,12 @@ $test_os = [
     'macos-15-intel', // bin/spc for x86_64
     'macos-15', // bin/spc for arm64
     // 'ubuntu-latest', // bin/spc-alpine-docker for x86_64
-    // 'ubuntu-22.04', // bin/spc-gnu-docker for x86_64
+    'ubuntu-22.04', // bin/spc-gnu-docker for x86_64
     // 'ubuntu-24.04', // bin/spc for x86_64
-    // 'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
+    'ubuntu-22.04-arm', // bin/spc-gnu-docker for arm64
     // 'ubuntu-24.04-arm', // bin/spc for arm64
     // 'windows-2022', // .\bin\spc.ps1
-    'windows-2025',
+    // 'windows-2025',
 ];
 
 // whether enable thread safe
@@ -43,14 +43,14 @@ $no_strip = false;
 $upx = false;
 
 // whether to test frankenphp build, only available for macOS and linux
-$frankenphp = true;
+$frankenphp = false;
 
 // prefer downloading pre-built packages to speed up the build process
 $prefer_pre_built = false;
 
 // If you want to test your added extensions and libs, add below (comma separated, example `bcmath,openssl`).
 $extensions = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'curl',
+    'Linux', 'Darwin' => 'curl,swoole',
     'Windows' => 'intl',
 };
 
@@ -66,7 +66,7 @@ $with_suggested_libs = true;
 
 // If you want to test extra libs for extensions, add them below (comma separated, example `libwebp,libavif`). Unnecessary, when $with_suggested_libs is true.
 $with_libs = match (PHP_OS_FAMILY) {
-    'Linux', 'Darwin' => 'krb5',
+    'Linux', 'Darwin' => 'krb5,libiouring',
     'Windows' => '',
 };
 
